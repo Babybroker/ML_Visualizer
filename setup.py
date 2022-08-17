3be4dc2762236d55bd8684474cf2f25f36a647a2
301630 <kevin.d@live.nl>
2022-08-17 10:11:58 +0200

    Init commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MLVisualizer",
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires = ['seaborn', 'scikit-learn'],
+
+)
+
